cd634bbc026cdeeb71f80053e5083ac21b85b2a3
supervip <172490109+kkgit2008@users.noreply.github.com>
2025-06-26 11:07:13 +0800

    更新 inflaterUtils.kt
    回滚到可用版本

--- utilities/xml-inflater/src/main/java/com/itsaky/androidide/inflater/utils/inflaterUtils.kt
+++ utilities/xml-inflater/src/main/java/com/itsaky/androidide/inflater/utils/inflaterUtils.kt
@@ -64,21 +64,15 @@ fun newAttribute(
 @JvmOverloads
 fun newAttribute(
   view: IView? = null,
-  namespace: INamespace? = null,
+  namespace: INamespace? = INamespace.ANDROID,
   name: String,
   value: String
 ): IAttribute {
-  val ns = namespace ?: when {
-    name.startsWith("android:") -> INamespace.ANDROID
-    name.startsWith("app:") -> NamespaceImpl("app", "http://schemas.android.com/apk/res-auto")
-    else -> INamespace.ANDROID // fallback
-  }
-
   val componentFactory = lookupComponentFactory()
   if (componentFactory != null && view != null) {
-    return componentFactory.createAttr(view, ns, name, value)
+    return componentFactory.createAttr(view, namespace, name, value)
   }
-  return AttributeImpl(ns, name, value)
+  return AttributeImpl(namespace, name, value)
 }
 
 /**
